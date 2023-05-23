d8021122b417db6146e0a731f8f87aad901b4f60
Matheus Antonio da Silva <matheus.silva@projedata.com.br>
2023-05-23 09:48:19 -0300

    lesson 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -4,3 +4,12 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+describe("Central de Atendimento ao Cliente TAT", () => {
+  it('Verifica o título da aplicação', () => {
+    cy.visit('src/index.html');
+
+    cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+    cy.title().should("to.equal", "Central de Atendimento ao Cliente TAT");
+  });
+});
